a06011a6e7439a29c9cfca43382c720bec5167a5
nikit <nikitaaa6@yandex.ru>
2023-01-04 19:14:09 +0800

    24/12/22

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //  задача №1
+        int age=25;
+        if (age>=18){
+        System.out.println("hooray for you 18");
+        }
+        if (age<18) {
+        System.out.println("the age of majority has not yet come");
+        }
+
+        //  задача №2
+        int age2=8;
+        if (age2>=7 && age2<=17 ){  System.out.println("the child goes to school");
+        }
+        if (age2>=18 && age2<=23){  System.out.println("he has already graduated from school and can go to university");
+        }
+        if (age2>=24){  System.out.println("he graduated from university and it's time for him to look for his first job");
+        }
+
+        //  задача №3
+
+    }
+}
